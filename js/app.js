b697b9c11950c5bec6c6f2ba3e7aa20528d1106d
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-06-22 03:20:25 +0000

    get results working

--- js/app.js
+++ js/app.js
@@ -6,45 +6,33 @@ $(function(){
     
     // gets data from youtube
 function getResults(searchTerm) {
-    $.getJSON('https://www.googleapis.com/youtube/v3/search') , 
-        {
-            'part': 'snippet',
-            'key': 'AIzaSyC6qJzZE4VTLYD2X8URY7qNv_Hu68yQYW8',
-            'q': searchTerm
-        }
+    $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6qJzZE4VTLYD2X8URY7qNv_Hu68yQYW8&q=' + searchTerm, function(data){
+        console.log(data);
+    })   
+
 };    
         
-
   // shows search results and thumbnail images
 function showResults(results){
     var html = "";
-    $.each(results, function(index,value){
+    $.each(results, function(index,videos){
         
-        html += '<p>' + value.Title + '</p>';
-        console.log(value.Title);
+    //    html += '<li><p>' + video.snippet.Title + '</p>< target='_blank' href='https:/www.youtube.com/watch?v=' + video.id.videoId + <img src='" + video.snippet.thumbnails.high.url + "'/>/a></li>';
+        
+        console.log(video.snippet.Title);
     });
     
     $('#search-results ul').html(html);
 }
 
-  // use search term    
-$(function(){
+  // use search term   
+//$(function(){
   $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#query').val();
-    $.getJSON('https://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
-      showResults(data.Search);
-    });
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        getResults(searchTerm);
   });
-});
-
-$(function(){
-  $('#search-term').submit(function(event){
-    event.preventDefault();
-    var searchTerm = $('#query').val();
-    getRequest(searchTerm);
-  });
-});
+//});
 
 
 });  
