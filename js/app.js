666d03a6c0b7bb7669ecb15c778b3fab9a13dc4b
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-06-23 01:26:42 +0000

    thumbnails showing and link to youtube site

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
-$(function(){
-    
-    // APP Requirements
+$(function() {
+
+    // Requirements
     // Accept a user search term Get JSON from the YouTube API based on the user
     // search term *Display the thumbnail image of the returned videos
-    
+
     // gets data from youtube
-function getResults(searchTerm) {
-    $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6qJzZE4VTLYD2X8URY7qNv_Hu68yQYW8&q=' + searchTerm, function(data){
-        console.log(data);
-    })   
+    function getResults(searchTerm) {
+        $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6qJzZE4VTLYD2X8URY7qNv_Hu68yQYW8&q=' + searchTerm, function(data) {
+            console.log(data);
+            showResults(data); 
+        });
+    }
+    // shows search results and thumbnail images
+    function showResults(data) {
+                console.log(data);
+                
+                $.each(data.items, function(index, video) {
+                    console.log(video);
+                    var videoId = video.id.videoId;
+                    var thumbnails = video.snippet.thumbnails.medium.url;
+                    $('ul').append('<li>' + '<p>' + video.snippet.title + '</p>' +
+                    '<a target="_blank" href="https://www.youtube.com/watch?v=' + videoId + '"><img src= "' + thumbnails +'"/></a></li>');
+                });
+            }
 
-};    
-        
-  // shows search results and thumbnail images
-function showResults(results){
-    var html = "";
-    $.each(results, function(index,videos){
-        
-    //    html += '<li><p>' + video.snippet.Title + '</p>< target='_blank' href='https:/www.youtube.com/watch?v=' + video.id.videoId + <img src='" + video.snippet.thumbnails.high.url + "'/>/a></li>';
-        
-        console.log(video.snippet.Title);
-    });
-    
-    $('#search-results ul').html(html);
-}
-
-  // use search term   
-//$(function(){
-  $('#search-term').submit(function(event){
+    // use search term   
+    //$(function(){
+    $('#search-term').submit(function(event) {
         event.preventDefault();
         var searchTerm = $('#query').val();
         getResults(searchTerm);
-  });
-//});
+    });
+    //   console.log(showResults);
+    //});
 
 
-});  
+}); //end of jQuery
